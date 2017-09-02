03f14e70195be77f18192567901aa48e1a20eaa1
Yaro Shkvorets <shkvorets@gmail.com>
2017-09-02 00:58:56 -0400

    typo

--- bundle.js
+++ bundle.js
@@ -14625,7 +14625,7 @@
         w = i.tileLayer("http://[v].tile.opencyclemap.org/cycle/[z]/[x]/[y].png", {
 						attribution: '© <a href="https://www.openstreetmap.org/copyright/en">OpenCycleMap</a> contributors'
 					}),
-				d = i.tileLayer("https://tools.geofabrik.de/osmi/tiles/routing_i/{z}/{x}/{y}.png", {});
+				d = i.tileLayer("https://tools.geofabrik.de/osmi/tiles/routing_i/{z}/{x}/{y}.png", {}),
         m = i.tileLayer("https://d6a1v2w10ny40.cloudfront.net/v0.1/{z}/{x}/{y}.png", {});
 				e.exports = {
 					defaultState: {
