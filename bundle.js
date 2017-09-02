3252ec6bec607376431e8b28cd366c4c2ebf5755
Yaro Shkvorets <shkvorets@gmail.com>
2017-09-02 01:35:29 -0400

    fixed cyclemap

--- bundle.js
+++ bundle.js
@@ -14622,7 +14622,7 @@
 				l = i.tileLayer("https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png", {
 						attribution: '© <a href="https://www.openstreetmap.org/copyright/en">OpenStreetMap</a> contributors'
 					}),
-        w = i.tileLayer("https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png", {
+        w = i.tileLayer("https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=62e8ebfa9967458e8d06c86e92001c63", {
 						attribution: '© <a href="https://www.openstreetmap.org/copyright/en">OpenCycleMap</a> contributors'
 					}),
 				d = i.tileLayer("https://d6a1v2w10ny40.cloudfront.net/v0.1/{z}/{x}/{y}.png", {});
